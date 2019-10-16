221e06f0abe246a1ba9798fcfe679458f45cc95e
vonaxl <vonaxlcorso@gmail.com>
2019-10-16 15:08:11 +0200

    structure and edits

---
+++ assets/js/myscript.js
@@ -0,0 +1,6 @@
+var nome = prompt("Inserisci il tuo NOME");
+var cognome = prompt("Inserisci il tuo COGNOME");
+var colpref = prompt("Il tuo colore preferito?");
+var output = nome+cognome+colpref+"19";
+console.log(output);
+document.getElementById("outp").innerHTML = output;
