4d20270904f7318d9ece68dbfeeff6cacbe70b84
vonaxl <vonaxlcorso@gmail.com>
2019-10-16 16:15:59 +0200

    used camelcase

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,7 +1,7 @@
 var nome = prompt("Inserisci il tuo NOME");
 var cognome = prompt("Inserisci il tuo COGNOME");
-var colpref = prompt("Il tuo colore preferito?");
-var output = nome+cognome+colpref+"19";
+var colPref = prompt("Il tuo colore preferito?");
+var output = nome+cognome+colPref+"19";
 console.log(output);
 document.getElementById("cnome").innerHTML = nome;
 document.getElementById("outp").innerHTML = output;
