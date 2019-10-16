a8b818432dd20b75bdcf30cddf10c8bf88fcc806
vonaxl <vonaxlcorso@gmail.com>
2019-10-16 16:14:11 +0200

    done

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -3,4 +3,5 @@ var cognome = prompt("Inserisci il tuo COGNOME");
 var colpref = prompt("Il tuo colore preferito?");
 var output = nome+cognome+colpref+"19";
 console.log(output);
+document.getElementById("cnome").innerHTML = nome;
 document.getElementById("outp").innerHTML = output;
